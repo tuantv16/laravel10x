b7091df16031844671232e6504895367b4db4cb5
tuantv16 <vantuan24121996@gmail.com>
2025-06-12 23:07:40 +0700

    test xem dc k

--- app/Http/Controllers/QueryOrmController.php
+++ app/Http/Controllers/QueryOrmController.php
@@ -20,6 +20,7 @@ class QueryOrmController extends Controller
      */
     public function index()
     {
+        //hahaha
         // Setting 1 user vào 1 role22
         //( Thêm 1 row vào bảng trung gian users_roles)
         // $data = $this->addUserRole(1,8);
