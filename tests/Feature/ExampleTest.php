bb05d4650e9d0d43eb98cc2dca9e705302def76f
tuantv16 <vantuan24121996@gmail.com>
2025-06-12 22:20:54 +0700

    update lan 2

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
-
+        // xin chào
         $response->assertStatus(200);
     }
 }
